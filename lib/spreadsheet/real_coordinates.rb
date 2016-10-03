6af226c3d787d6338f07ba0ebb80e2c23b0efd0e
thoran <code@thoran.com>
2016-10-04 00:42:58 +1100

    Committed automatically by git-importall.

---
+++ lib/spreadsheet/real_coordinates.rb
@@ -0,0 +1,26 @@
+# 2010.04.11
+
+require 'spreadsheet'
+
+module OneBased
+  module Spreadsheet
+    module Worksheet
+      
+      def [](row, column)
+        row(row - 1, column - 1)
+      end
+      
+      def []=(row, column, value)
+        row(row - 1)[column - 1] = value
+      end
+      
+    end
+  end
+end
+
+Spreadsheet::Worksheet.send(:include, OneBased::Spreadsheet::Worksheet)
+
+workbook = Spreadsheet.open('../../templates/3 - Request for Activations v1 - SPs.xls')
+worksheet = workbook.worksheet(0)
+worksheet[6,2] = '2010-04-11'
+workbook.write('out.xls')
